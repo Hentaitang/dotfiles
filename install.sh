1a507afca7d76cdefe2227e84a57b980792f16d2
Hentaitang <mz374427166@gmail.com>
2025-03-18 16:45:51 +0800

    change install

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ fi
 
 if ! command -v brew &> /dev/null; then
        echo "Installing Homebrew..."
+       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 echo "Updating Homebrew..."
 brew update
