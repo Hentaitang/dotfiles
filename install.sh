5e9134d1b60c2f27f7782ff474d2edf059b71291
Hentaitang <mz374427166@gmail.com>
2025-03-21 17:09:55 +0800

    add nvm & install node

--- install.sh
+++ install.sh
@@ -12,6 +12,17 @@ if [ ! -d ~/.oh-my-zsh ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+if [ ! -d "$HOME/.nvm" ]; then
+	echo "Installing nvm..."
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+fi
+
+if command -v node > /dev/null; then
+	echo "Node.js installed: $(node --version)"
+else
+	nvm install node
+fi
+
 if ! command -v brew &> /dev/null; then
        echo "Installing Homebrew..."
        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
